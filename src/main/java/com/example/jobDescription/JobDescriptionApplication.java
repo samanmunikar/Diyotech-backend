fadeb53d1c7426e8d084ed0e3c44b2a13bf798fa
Rupesh <Rxkst244@mail.rmu.edu>
2023-06-15 10:11:51 -0400

    first commit

---
+++ src/main/java/com/example/jobDescription/JobDescriptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobDescription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDescriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDescriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobDescription/JobDescriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobDescription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDescriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
